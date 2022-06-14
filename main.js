b6ae0fd6cd60a32fd371bc54ffb4a9794adbf2ae
LiorDavid1996 <liordavid356@gmail.com>
2022-06-14 23:16:15 +0300

    first.commit

---
+++ main.js
@@ -0,0 +1,47 @@
+// 14.06.22-משימות מערכים js חדש
+// question1
+// function sortArray(str){
+// console.log(str.sort());
+// }sortArray(["lior","aviel","natan","daniel","chen","or"])
+// // /question2
+// function SortingMinToMaxNum(num){
+//     console.log(num.sort((a,b)=> a-b));
+// }SortingMinToMaxNum([12,33,22,3,,56,77])
+// // question3
+// function SortingMaxToMinNum(num){
+//     console.log(num.sort((a,b)=> b-a));
+// }SortingMaxToMinNum([12,33,22,3,,56,77])
+// // question4
+// var arrayNames=[]
+// function sortNamesOfStudent(){
+// var user=document.getElementById("inputNamesFromUser")
+// arrayNames.push(user.value)
+// arrayNames.sort()
+// arrayNames.join("'-'")
+// console.log(arrayNames)
+
+// ;
+// }
+// // question5
+// var arrayRndAge = []
+// function rndArrayOfAge(){
+// for (i = 0; i < 10; i++) {
+//     var rnd = Math.floor(Math.random() * 55)
+//     arrayRndAge.push(rnd)
+//     arrayRndAge.sort((a, b) => a - b)
+// } console.log(arrayRndAge)
+// }rndArrayOfAge()
+
+// question6
+// arrayNames.join("-")
+// console.log(arrayNames)
+
+// question7
+var myh1=document.getElementById("h1_1")
+var array=["facebook","apel","asus","google","amazon","sony","microsoft","jbl","wix",]
+for(i=0;i<array.length;i++){
+    myh1.innerHTML+=array[i]+"<br>"
+}
+var myarray=[]
+myarray.push(img_4.jpg)
+document.write(myarray)
